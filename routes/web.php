71bacd1d9654a3921aa489ff6f68c35fdcbd8ff9
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-09-10 13:33:27 -0300

    define routes for methods in MainController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'Hello world! MathX';
-});
+// Main
+Route::get('/', [MainController::class, 'home'])->name('homePage');
+
+// Rota para gerar os exercícios
+Route::post('/generate-exercises', [MainController::class, 'generateExercises'])->name('generated');
+
+// Rota para mostrar os exercícios na tela
+Route::get('/print-exercises', [MainController::class, 'printExercises'])->name('print');
+
+// Rota para exportar os exercícios
+Route::get('/export-exercises', [MainController::class, 'exportExercises'])->name('export');
+
